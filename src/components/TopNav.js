7dcb0720b058d8b713658785cdc2fa4c28a227b6
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-12 23:59:21 +0200

    Top-Nave edits #3

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -3,15 +3,16 @@ import {AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag} from 'react-i
 
 const TopNav = () =>{
   return (
-    <div className='max-w-[1520]mx-auto flex justify-between items-center p-4'>
+    <div className='max-w-[1520 px]mx-auto flex justify-between items-center p-4'>
      <div className='flex items-center' >
      <div className='cursor-pointer' >
             <AiOutlineMenu size={25}/>
     </div>
            
-    <h1 className='text-2xl sm:text-3xl lg:text-4xl'>Yum
+    <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Yum
     <span> Eats </span>
     </h1>
+    <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'> </div>
     </div>
     </div>
   )

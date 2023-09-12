82dea98540a7e575a633dd237a3a2a32f5897232
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-13 00:24:52 +0200

    Top-Nave Bar #4

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -10,10 +10,15 @@ const TopNav = () =>{
     </div>
            
     <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Yum
-    <span> Eats </span>
+    <span>Eats</span>
     </h1>
-    <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'> </div>
+    <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'> 
+        <p className='bg-orange-700 text-white rounded-full p-2 text-bold'  >Free</p>
+        <p className='p-2 text-bold'>Delivery</p>
     </div>
+    </div>
+        <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]' />
+        <AiOutlineSearch size={25}/>
     </div>
   )
 }

74dc16e1006420ad712b426c5910bc9b169bd2cd
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-15 09:06:52 +0200

    to implement the menu

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai'
-import {BsFillCartFill, BsPerson} from 'react-icons/bs'
+import { BsFillCartFill, BsPerson } from 'react-icons/bs'
 
 const TopNav = () => {
-   const [sideNav, setSideNav] = useState(false)
+  const [sideNav, setSideNav] = useState(false)
+  console.log (sideNav);
   return (
     <div className='max-w-[1520 px]mx-auto flex justify-between items-center p-4'>
       <div className='flex items-center' >
-        <div onClick={()=> setSideNav(sideNav)} className='cursor-pointer' >
+        <div onClick={() => setSideNav(!sideNav)} className='cursor-pointer' >
           <AiOutlineMenu size={25} />
         </div>
-
         <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Yum
           <span>Eats</span>
         </h1>
@@ -19,24 +19,35 @@ const TopNav = () => {
           <p className='p-2 text-bold'>Delivery</p>
         </div>
       </div>
-      <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
+      <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] 
+      sm:w-[400px] lg:w-[500px]'>
         <AiOutlineSearch size={25} />
         <input className='bg-transparent p-2 w-full focus:outline-none'
           type='text'
           placeholder='search meals'
         />
       </div>
-        <button className='bg-orange-700 text-white hidden md:flex items-center py-2 rounded-full'>
-         <BsFillCartFill size={20}/> Cart
-        </button>
-          {
-            sideNav ? (
-              <div className='bg-black/60 fixed w-full h-screen z-10 top-0 left-0'/>
-            ): ("")
-          }
-        <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-200'/>
-    </div>
+      <button className='bg-orange-700 text-white hidden md:flex items-center py-2 rounded-full'>
+        <BsFillCartFill size={20} /> Cart
+      </button>
+      {
+        sideNav ? (
+          <div className='bg-black/60 fixed w-full h-screen z-10 top-0 left-0' />
+        ) : ("")
+      }
+      <div 
+        className= {
+          sideNav 
+          ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300" 
+          : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+          >
+            < AiOutlineClose onClick={() => setSideNav(!sideNav)}size={25}
+            className='absolue' right-4 top-4 cursor-pointer/>
+            
+          </div>
+          </div>
   )
-} 
+}
 
-export default TopNav  
+export default TopNav

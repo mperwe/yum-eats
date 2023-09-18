ca6c52587b77b38b3187c1cf686d8b3fcb123036
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-18 18:03:36 +0200

    arros edits

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {BsChevronCompactLeft,BsChevronCompactRight }from'react-icons/bs'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
 const Featured = () => {
   const sliders = [
     {
@@ -11,11 +11,14 @@ const Featured = () => {
   return (
     <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative'>
       <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-        style={{backgroundImage: 'url(${sliders}[currentIndex].url})' }}
+        style={{ backgroundImage: 'url(${sliders}[currentIndex].url})' }}
       />
-      <div className= 'absolute top-[50%] -translate-x-0 traslate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
-      <BsChevronCompactLeft />
-        </div>
+      <div className='absolute top-[50%] -translate-x-0 traslate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+        <BsChevronCompactLeft />
+      </div>
+      <div className='absolute top-[50%] -translate-x-0 traslate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+        <BsChevronCompactRight />
+      </div>
     </div>
   )
 }

24bfbcfb412a67291ccef5a12820baf86e0a7f1b
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-19 22:45:44 +0200

    slider updates

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react'
+ import React, { useState } from 'react'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
+import {RxDotFilled} from 'react-icons/rx'
+
 const Featured = () => {
   const sliders = [
     {
       url: "foodmaterials\food1.jpg"
     },
 
+    {
+      url: "foodmaterials\food1.jpg"
+    },
+
+    {
+      url: "foodmaterials\food1.jpg"
+    },
+
   ]
   const [currentIndex, setCurrentIndex] = useState(0)
-  const prevSlider =()=> {
+  const prevSlider = () => {
+
     const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide ? sliders.length - 1 : currentIndex -1
+    const newIndex = isFirstSlide ? sliders.length - 1 : currentIndex - 1
     setCurrentIndex(newIndex)
   }
-   
-  const nextSlider =()=> {
-    const isLastSlide = currentIndex === sliders.length
-    const newIndex = isLastSlide ? 0: currentIndex + 1
-    setCurrentIndex(newIndex)  
+
+  const nextSlider = () => {
+    const isLastSlide = currentIndex === sliders.length - 1
+    const newIndex = isLastSlide ? 0 : currentIndex + 1
+    setCurrentIndex(newIndex) 
   }
-
-
+        const moveToSlide = (slideIndex) => {
+                setCurrentIndex(slideIndex)    
+        }
 
   return (
     <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'>
@@ -29,12 +41,21 @@ const Featured = () => {
       />
 
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 traslate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlider}/>
+        <BsChevronCompactLeft onClick={prevSlider} />
 
       </div>
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 traslate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlider} />
       </div>
+        <div className='flex top-4 justify-center py-2'>
+          <RxDotFilled/>
+        </div>
+        {
+          sliders.map((sliderItems, slideIndex)=>(
+            
+            <div className='text-2xl cursor-pointer'></div>
+          ))
+        }
     </div>
   )
 }

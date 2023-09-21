40d32be9d8383f1daf3892707b58292d627b37ce
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-21 18:53:30 +0200

    fixing page images

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,6 +1,6 @@
- import React, { useState } from 'react'
+import React, { useState } from 'react'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
-import {RxDotFilled} from 'react-icons/rx'
+import { RxDotFilled } from 'react-icons/rx'
 
 const Featured = () => {
   const sliders = [
@@ -25,16 +25,16 @@ const Featured = () => {
   const nextSlider = () => {
     const isLastSlide = currentIndex === sliders.length - 1
     const newIndex = isLastSlide ? 0 : currentIndex + 1
-    setCurrentIndex(newIndex) 
+    setCurrentIndex(newIndex)
   }
-        const moveToSlide = (slideIndex) => {
-                setCurrentIndex(slideIndex)    
-        }
+  const moveToNextSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex)
+  }
 
   return (
     <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'>
       <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-        style={{ backgroundImage: `url(${sliders[currentIndex].url})`}}
+        style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}
       ></div>
 
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 traslate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
@@ -44,18 +44,21 @@ const Featured = () => {
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 traslate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlider} />
       </div>
-        <div className='flex top-4 justify-center py-2'>
-          <RxDotFilled/>
-        </div>
-        {
-          sliders.map((sliderItems, slideIndex)=>(
-            
-            <div className='text-2xl cursor-pointer' key={slideIndex}>
-
-            </div>
-          ))
-        }
+      <div className='flex top-4 justify-center py-2'>
+       
+      </div>
+      {
+        sliders.map((sliderItems, slideIndex) => (
+          <div
+            key={slideIndex} 
+            onClick={() => moveToNextSlide(slideIndex) }
+            className='text-2xl cursor-pointer'>
+            <RxDotFilled />
+          </div>
+        ))
+      }
     </div>
+  
   )
 }
 

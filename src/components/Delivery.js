f1da3e857855776151f02f80c1efcdb182b8271d
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-22 16:26:25 +0200

    Update Quick Delivery App

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -1,11 +1,22 @@
-import React from 'react'
+    import React from 'react'
 
 const Delivery = () => {
-  return (
+  return ( 
     <div className='w-full bg-white py-16 px-4'> 
         <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick Delivery App</h3>
         <div className='w-[1520px] mx-auto grid md:grid-cols-2'>
             <img className='w-[550px] mx-auto my-4'src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp'/>
+            <div className='flex flex-col justify-center'>
+            <p className='text-[#00df9a] font-bold '>Get the App</p>
+            <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Limitless Convenience on-Demand</h1>
+            <p>
+            Welcome to YumEats Shop! We offer a variety of delicious pizzas to satisfy all tastes. Order online or give us a call to enjoy our mouthwatering slices. Thank you for choosing us! 🍕🎉
+            </p>
+            <button className='bg-black text text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started </button>
+
+            </div>
+
+
         </div>
     </div>
   )

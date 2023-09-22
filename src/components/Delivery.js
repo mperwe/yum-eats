f4c3ed8cf08e88f01e15898e751d53864e9fae7a
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-22 12:35:58 +0200

    Add quick delivery app

---
+++ src/components/Delivery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Delivery = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'> 
+        <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick Delivery App</h3>
+    </div>
+  )
+}
+
+export default Delivery

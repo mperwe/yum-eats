4cea6867c3af2885d8f352852879b63f0f1f8c8f
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-22 18:31:13 +0200

    Edit Delivery page size

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -36,23 +36,20 @@ const Featured = () => {
   return (
     <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'>
       <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-        style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}
-      ></div>
+        style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}></div>
 
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 traslate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlider} />
-
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 traslate-y-[-50%]
+       left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlider}/>
       </div>
+      
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 traslate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlider} />
       </div>
-      <div className='flex top-4 justify-center py-2'>
-      {
+
+      <div className='flex top-4 justify-center py-2'> {
         sliders.map((sliderItems, slideIndex) => (
-          <div
-            key={slideIndex} 
-            onClick={() => moveToNextSlide(slideIndex) }
-            className='text-2xl cursor-pointer'> 
+          <div key={slideIndex} onClick={() => moveToNextSlide(slideIndex)} className='text-2xl cursor-pointer'> 
             <RxDotFilled />
           </div>
         ))

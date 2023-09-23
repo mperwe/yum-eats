7e9b88a2304b51bb54b00e9774a16ee75772be94
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-23 19:04:45 +0200

    import TopPicks componet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Delivery from "./components/Delivery";
 import Featured from "./components/Featured";
 import TopNav from "./components/TopNav";
+import TopPicks from "./components/TopPicks"
 function App() {
   return (
     <div className="App">

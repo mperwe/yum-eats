13703981ec2dda079c4c4ca05f74e2a470fb2d0f
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-26 11:54:11 +0200

    define the image

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -14,6 +14,7 @@ const TopPicks = () => {
                     <div className='rounded-3xl relative'>
                         <div className='absolute w-full h-full bg-black/50 rounded-3xl text-white'>
                         <p className='px-2'>{item.title}</p>
+                        <button className='border-dotted border-white test-white mx-2 absolute bottom-4'>Add To Cart</button>
                         </div>
                         <img className='h-[100px] w-full object-cover rounded-3xl cursor-pointer hover:scale-105 ease-out duration-300'
                         src={item.img}/>

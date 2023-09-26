2e0e85a14eabbbf54727a5a51c02571f7fc94e9f
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-26 19:43:25 +0200

    styling the image slides

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -8,22 +8,25 @@ const TopPicks = () => {
     <>
     <h1 className='text-orange-500 font-bold text-2xl text-center py-2'> Top Picks</h1>
     <div className='hidden lg:flex max-w[1520] m-auto py-2 px-=2'>
+        <Splide options={{perPage: 4, gap: "0.5rem"}}>
         {
             topPicks.map((item)=>{
                 return( 
+                    <SplideSlide>
                     <div className='rounded-3xl relative'>
                         <div className='absolute w-full h-full bg-black/50 rounded-3xl text-white'>
                         <p className='px-2'>{item.title}</p>
                         <button className='border-dotted border-white test-white mx-2 absolute bottom-4'>Add To Cart</button>
                         </div>
-                        <img className='h-[100px] w-full object-cover rounded-3xl cursor-pointer hover:scale-105 ease-out duration-300'
+                        <img className='h-[200px] w-full object-cover rounded-3xl cursor-pointer hover:scale-105 ease-out duration-300'
                         src={item.img}/>
                      </div>
+                     </SplideSlide>
         
                 )
             }) 
         }
-
+        </Splide>
     </div>
     </>
   )

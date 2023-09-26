4408df6711c39b4bf6030e26e164473710d67a22
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-26 22:51:39 +0200

    implement price on images

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -16,10 +16,13 @@ const TopPicks = () => {
                     <div className='rounded-3xl relative'>
                         <div className='absolute w-full h-full bg-black/50 rounded-3xl text-white'>
                         <p className='px-2'>{item.title}</p>
+                        <p>{item.price}</p>
                         <button className='border-dotted border-white test-white mx-2 absolute bottom-4'>Add To Cart</button>
                         </div>
                         <img className='h-[200px] w-full object-cover rounded-3xl cursor-pointer hover:scale-105 ease-out duration-300'
-                        src={item.img}/>
+                        src={item.img}
+                        alt={item.title}
+                        />
                      </div>
                      </SplideSlide>
         

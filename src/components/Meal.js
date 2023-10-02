d419f0783cb12e4fc74dbbe5d1d71e93c424f3a7
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-02 14:57:23 +0200

    implement flters functionality

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -4,6 +4,13 @@ import { mealData } from '../data/data'
 
 const Meal = () => {
     const [foods, setFoods] = useState(mealData)
+    const filterCat =(category)=>{
+        setFoods(
+            mealData.filter((item)=>{
+                return item.category === category
+            })
+        )
+    }
   return (
     <div className='max-w-[1520px] m-auto px-4 py-12'>
         <h1 className='text-orange-500 font-bold text-2xl text-center py-2'> 

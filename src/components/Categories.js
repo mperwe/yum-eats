d141451e6099f21789a1c304a00a60e29c71b5bc
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-03 16:46:11 +0200

    implement Category component

---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {categories } from '../data/data'
+
+const Categories = () => {
+    console.log(categories)
+  return (
+    <div> Categories</div>
+  )
+}
+
+export default Categories

2058ed6ef7c4f2ebff7d63dbc42c0983f7a26375
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-03 18:03:07 +0200

    implement NewsLetter component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,7 +4,7 @@ import {categories } from '../data/data'
 const Categories = () => {
     console.log(categories)
   return (
-    <div className='max-w-[1520] m-auto px-4 py-4'> 
+    <div className='max-w-[1520px] m-auto px-4 py-4'> 
         <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>
         Trending Categories</h1>
         <div className='grid grid-cols-2 md:grid-cols-6 gap-5 py-5 px-2'>
---
+++ src/components/NewsLetter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NewsLetter = () => {
+  return (
+    <div className='max-w-[1520px] m-auto text-white px-4 bg-[#24262b]'>    
+      <div className='mx-auto grid lg:grid-cols-3'>
+        <div className='lg:col-span-2 my-4'>
+            <h1>Need advice on how to improve your flow?</h1>
+            <p>Sign Up to join our newletter an dstay up to date.</p>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default NewsLetter

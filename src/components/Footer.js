f537de0ad863739f673a830e1bb025d8bd47fe26
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-04 06:50:34 +0200

    stlye footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { FaDribbbleSquare, FaFacebookSquare, FaGithubSquare, FaInstagramSquare, 
 const Footer = () => {
   return (
     <div className='max-w-[1520px] m-auto px-4 py-2 bg-[#24262b]'>
-        <div className='py-16 px-4 grid lg:grid-cols3 gap-8 text-gray-300'>
+        <div className='py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
             <div>
                 <h1 className='w-full text-3xl font-bold text-orange-500'>YumEats</h1>
                 <p>
